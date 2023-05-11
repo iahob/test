bbcc5157a5bfdc1af64dd29a24e34a0ee370302d
yuhaibo <iahob@icloud.com>
2023-05-11 11:12:50 +0800

    add

--- add-pre-commit.sh
+++ add-pre-commit.sh
@@ -5,8 +5,7 @@ echo '
 
 echo "proto builder start"
 
-# docker run -v $(pwd):/go/src/app -w /go/src/app yuwenhaibo/proto-builder sh -c "protoc --go-grpc_out=paths=source_relative:. --go_out=paths=source_relative:. *.proto;"
-docker run -v $(pwd):/go/src/app -w /go/src/app yuwenhaibo/proto-builder sh builder.sh
+sh builder.sh
 
 if [[ ! $(git diff --name-only) ]]; then
         echo "Needless To add"
--- builder.sh
+++ builder.sh
@@ -1,3 +1,5 @@
  #/bin/sh
+docker run -v $(pwd):/go/src/app -w /go/src/app yuwenhaibo/proto-builder sh -c '
+  protoc --go-grpc_out=paths=source_relative:. --go_out=paths=source_relative:. *.proto;
+'
 
-protoc --go-grpc_out=paths=source_relative:. --go_out=paths=source_relative:. *.proto;
