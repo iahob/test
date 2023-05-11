5627f2a07bdd0dbf282ef6b984652e829565630e
yuhaibo <iahob@icloud.com>
2023-05-11 11:46:06 +0800

    add

--- builder.sh
+++ builder.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # shellcheck disable=SC2046
 docker run --rm -v $(pwd):/go/src/app -w /go/src/app yuwenhaibo/proto-builder sh -c '
-  protoc --go-grpc_out=paths=source_relative:./api --go_out=paths=source_relative:./api hello.proto;
+  protoc --go-grpc_out=paths=source_relative:./api --go_out=paths=source_relative:./api ./api/hello.proto;
 '
 sleep 2
