4dbdbe46d51e0d0e3f987c74a456a5b478a25f3e
yuhaibo <iahob@icloud.com>
2023-05-11 11:28:06 +0800

    add

--- builder.sh
+++ builder.sh
@@ -3,4 +3,4 @@
 docker run --rm -v $(pwd):/go/src/app -w /go/src/app yuwenhaibo/proto-builder sh -c '
   protoc --go-grpc_out=paths=source_relative:. --go_out=paths=source_relative:. *.proto;
 '
-
+sleep 2s
