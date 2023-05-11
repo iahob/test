e6397412301eaafcd558eb990abb59292d71367c
yuhaibo <iahob@icloud.com>
2023-05-11 13:46:46 +0800

    add

--- githooks/add-pre-commit.sh
+++ githooks/add-pre-commit.sh
@@ -5,13 +5,13 @@ echo '
 
 echo "proto builder start"
 
-sh builder.sh
+sh githuooks/builder.sh
 
 if [[ ! $(git diff --name-only) ]]; then
         echo "Needless To add"
     else
         git add *.pb.go
 fi
-'>.git/hooks/pre-commit
+'>../.git/hooks/pre-commit
 
-chmod 775 .git/hooks/pre-commit
+chmod 775 ../.git/hooks/pre-commit
