a7ce692530907c949cfc48bd804b7535d0400fb5
yuhaibo <iahob@icloud.com>
2023-05-11 14:35:32 +0800

    add

--- githooks/builder.sh
+++ githooks/builder.sh
@@ -4,7 +4,7 @@ echo "current dir:`pwd`"
 for file in api/*
 do
    if [[ $file = ~\.proto$ ]] ; then
-    echo "current file:$file"
+    echo "current file:${file}"
     docker run --rm -v $(pwd):/go/src/app -w /go/src/app yuwenhaibo/proto-builder sh -c '
       protoc --go-grpc_out=paths=source_relative:. --go_out=paths=source_relative:. *.proto;
     '
