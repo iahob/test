0fd709e81e310b9ce45a91589ec31e701157d0fe
yuhaibo <iahob@icloud.com>
2023-05-11 14:02:42 +0800

    add

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"context"
+	"time"
+
+	hello "test/api"
+)
+
+type Server struct {
+	hello.UnimplementedHelloServiceServer
+}
+
+func (s *Server) SayHello(context.Context, *hello.HelloRequest) (*hello.HelloResponse, error) {
+	return &hello.HelloResponse{
+		Reply: "hello",
+		Code:  "100",
+		Times: time.Now().Unix(),
+	}, nil
+}
+
 func main() {
 
 }
