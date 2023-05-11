94007474f304572a9fc96187c4ad4e42a66ddc61
yuhaibo <iahob@icloud.com>
2023-05-11 13:56:46 +0800

    add

--- githooks/builder.sh
+++ githooks/builder.sh
@@ -2,6 +2,6 @@
 # shellcheck disable=SC2046
 echo "current dir:`pwd`"
 docker run --rm -v $(pwd):/go/src/app -w /go/src/app yuwenhaibo/proto-builder sh -c '
-  protoc --proto_path=api --go-grpc_out=paths=source_relative:api --go_out=paths=source_relative:api api/hello.proto;
+  protoc --proto_path=api --go-grpc_out=paths=source_relative:api --go_out=paths=source_relative:api api/*.proto;
 '
 sleep 2
