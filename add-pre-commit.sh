ae98a111df5b20db56e6d99f8650274a361e0409
yuhaibo <iahob@icloud.com>
2023-05-11 11:17:31 +0800

    add

--- add-pre-commit.sh
+++ add-pre-commit.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/sh
 
 echo '
 #/bin/sh
--- builder.sh
+++ builder.sh
@@ -1,4 +1,5 @@
- #/bin/sh
+#!/bin/sh
+# shellcheck disable=SC2046
 docker run -v $(pwd):/go/src/app -w /go/src/app yuwenhaibo/proto-builder sh -c '
   protoc --go-grpc_out=paths=source_relative:. --go_out=paths=source_relative:. *.proto;
 '
