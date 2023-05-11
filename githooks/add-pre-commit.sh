7785a46f502ad8704a22364243dd00e70d21bdc3
yuhaibo <iahob@icloud.com>
2023-05-11 17:37:07 +0800

    add

--- githooks/add-pre-commit.sh
+++ githooks/add-pre-commit.sh
@@ -9,6 +9,7 @@ do
     if [[ "${file##*.}"x = "proto"x ]] ;
     then
       docker run --rm -v $(pwd):/go/src/app -w /go/src/app yuwenhaibo/proto-builder sh builder.sh
+      git add *.pb.go
       break
     else
       echo "skip"
@@ -16,11 +17,11 @@ do
     fi
 done
 
-if [[ ! $(git diff --name-only) ]]; then
-        echo "Needless To add"
-    else
-        git add *.pb.go
-fi
+#if [[ ! $(git diff --name-only) ]]; then
+#        echo "Needless To add"
+#    else
+#        git add *.pb.go
+#fi
 '>../.git/hooks/pre-commit
 
 chmod 775 ../.git/hooks/pre-commit
