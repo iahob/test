135c5efbfea18458044dba63a0c63134dd220c3d
yuhaibo <iahob@icloud.com>
2024-01-09 16:17:49 +0800

    update hooks

--- hooks/proto-builder.sh
+++ hooks/proto-builder.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-## 遍历API文件夹下的所有子文件夹
-#function builder() {
-#  for dir in $1/*/; do
-#    dir=${dir%*/}  # 删除末尾的斜杠
-#    for file in $dir/*.proto; do
-#      echo $file
-#      protoc --go-grpc_out=paths=source_relative:. --go_out=paths=source_relative:. $file
-#    done
-#  done
-#}
-
-home=$(pwd)/$1
-echo $home
-protoc -I=$home --go_out=plugins=grpc,paths=source_relative:$home $home/**/*.proto
-
-
-#builder $1
-
+# 递归函数，用于处理指定路径下的所有.proto文件
+function generate_protobuf() {
+  local directory="$1"
+  for file in "$directory"/*.proto; do
+    if [ -f "$file" ]; then
+      protoc --go-grpc_out=paths=source_relative:. --go_out=paths=source_relative:. "$file"
+    fi
+  done
+  for subdir in "$directory"/*/; do
+    if [ -d "$subdir" ]; then
+      generate_protobuf "$subdir"
+    fi
+  done
+}
+generate_protobuf $1
  
