a18ba9ae582431a4a1c1a2314bd95cbdaf4e45aa
yuhaibo <iahob@icloud.com>
2024-01-09 15:44:30 +0800

    update hooks

--- hooks/proto-builder.sh
+++ hooks/proto-builder.sh
@@ -5,6 +5,7 @@ function builder() {
   for dir in $1/*/; do
     dir=${dir%*/}  # 删除末尾的斜杠
     for file in $dir/*.proto; do
+      echo $file
       protoc --go-grpc_out=paths=source_relative:. --go_out=paths=source_relative:. $file
     done
   done
