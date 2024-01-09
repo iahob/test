09a371f9be6597e09fe2066ec777da08c55b1ac2
yuhaibo <iahob@icloud.com>
2024-01-09 15:48:23 +0800

    update hooks

--- hooks/proto-builder.sh
+++ hooks/proto-builder.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# 遍历API文件夹下的所有子文件夹
-function builder() {
-  for dir in $1/*/; do
-    dir=${dir%*/}  # 删除末尾的斜杠
-    for file in $dir/*.proto; do
-      echo $file
-      protoc --go-grpc_out=paths=source_relative:. --go_out=paths=source_relative:. $file
-    done
-  done
-}
+## 遍历API文件夹下的所有子文件夹
+#function builder() {
+#  for dir in $1/*/; do
+#    dir=${dir%*/}  # 删除末尾的斜杠
+#    for file in $dir/*.proto; do
+#      echo $file
+#      protoc --go-grpc_out=paths=source_relative:. --go_out=paths=source_relative:. $file
+#    done
+#  done
+#}
 
-builder $1
+protoc -I=./$1 --go_out=plugins=grpc,paths=source_relative:./$1 ./$1/**/*.proto
+
+
+#builder $1
 
  
